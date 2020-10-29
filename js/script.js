63ddc165624c32fab058f254d5b14a3d9458ef7f
jeremyv83 <jeremyvivie@hotmail.fr>
2020-10-29 14:18:11 +0100

    affichage du premier legumes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ var poeme = document.getElementById('poeme').textContent;
 var compteur = document.getElementById('compteur');
 compteur.innerHTML = poeme.length;
 
+var legumes = ["patate", "carotte", "aubergine", "haricot", "courgette","citrouille", "salade","lentilles","poivron","chou-fleur", "betterave"];
+
+console.log(legumes[0]);
